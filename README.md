41e8a002934453b1c688d8e6c733cc5f9a882443
jourzero <epaquet@192.168.10.192>
2016-10-19 19:55:54 -0400

    Minor update

--- README.md
+++ README.md
@@ -4,10 +4,12 @@ Burp app that passively detects WAF presence from HTTP responses.
 
 ## Current features
 * Run in the background and create passive scanner issues when WAF traces are detected.
+* Use Regex approach to search for matches in HTTP responses
+* Support searches in the headers only or in the whole response
+* Use separate keyword to highlight responses 
 
 ## TODO
-* Add WAF fingerprints/keywords
-* Use Regex approach
+* Add more WAF fingerprints/keywords
 * Externalize configuration of WAF detections
 * Integrate external research if possible
 
--- src/burp/BurpExtender.java
+++ src/burp/BurpExtender.java
@@ -3,12 +3,15 @@
  */
 package burp;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.*;
 import java.io.PrintWriter;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.commons.csv.*;
 
 public class BurpExtender implements IBurpExtender, IScannerCheck
 {
@@ -16,12 +19,13 @@ public class BurpExtender implements IBurpExtender, IScannerCheck
     private IExtensionHelpers helpers;
 
     private static final String EXT_NAME = "WAFDetect";
-    //private static final byte[] GREP_STRING = "ASP.NET".getBytes();
     private static HashMap wf = new HashMap(); 
-    //private static final byte[] INJ_TEST = "|".getBytes();
-    //private static final byte[] INJ_ERROR = "Unexpected pipe".getBytes();
     private PrintWriter stdout;
     private PrintWriter stderr;
+    private static List<WafFingerprint> wafFingerprints = new ArrayList<WafFingerprint>();
+    private static final String FP_FILENAME = "WafFingerprints.csv";   
+    private static final Boolean debug = false;
+    private static final Boolean info = true;
     
     //
     // implement IBurpExtender
@@ -29,43 +33,42 @@ public class BurpExtender implements IBurpExtender, IScannerCheck
     
     @Override
     public void registerExtenderCallbacks(final IBurpExtenderCallbacks callbacks)
-    {
-        // Create list 
-        wf.put("Incapsula WAF"    , "incap_ses");
-        wf.put("Incapsula CDN"    , "X-CDN: Incapsula");
-                
+    {                
         // keep a reference to our callbacks object
         this.callbacks = callbacks;
         
         // obtain an extension helpers object
         helpers = callbacks.getHelpers();
         
-        // set our extension name
+        // Set extension name
         callbacks.setExtensionName(EXT_NAME);
 
-        // obtain our output and error streams
+        // Obtain output and error streams
         stdout = new PrintWriter(callbacks.getStdout(), true);
         stderr = new PrintWriter(callbacks.getStderr(), true);
         
-        // write messages to output streams
+        // Write messages to output streams
         stdout.println("Started " + EXT_NAME);
-        callbacks.issueAlert("Started " + EXT_NAME);
-        //throw new RuntimeException("Hello exceptions");        
+        //callbacks.issueAlert("Started " + EXT_NAME);
+
+        // Load the WAF fingerprints from external file (in JAR file)
+        loadWafFingerprints();
+        stdout.println("DEBUG: Fingerprints loaded: " + wafFingerprints.toString());
         
-        // register ourselves as a custom scanner check
+        // Register ourselves as a custom scanner check
         callbacks.registerScannerCheck(this);
     }
     
     // helper method to search a response for occurrences of a literal match string
     // and return a list of start/end offsets
-    private List<int[]> getMatches(byte[] response, byte[] match)
+    private List<int[]> getMatches(byte[] response, byte[] match, int maxOffset)
     {
         List<int[]> matches = new ArrayList<int[]>();
 
         int start = 0;
         while (start < response.length)
         {
-            start = helpers.indexOf(response, match, true, start, response.length);
+            start = helpers.indexOf(response, match, true, start, maxOffset);
             if (start == -1)
                 break;
             matches.add(new int[] { start, start + match.length });
@@ -75,37 +78,57 @@ public class BurpExtender implements IBurpExtender, IScannerCheck
         return matches;
     }
 
-    //
     // implement IScannerCheck
-    //
     
+    /**
+     * Listen to HTTP traffic and identify WAF fingerprints
+     * @param baseRequestResponse
+     * @return "WAF Detected" as can issue logs
+     */
     @Override
     public List<IScanIssue> doPassiveScan(IHttpRequestResponse baseRequestResponse)
     {
-        List<IScanIssue> issues = new ArrayList<>(1);
-        //List<IScanIssue> issues = null;
-        Iterator it = wf.entrySet().iterator();
+        // Issue list to send to Burp as passive findings
+        List<IScanIssue> issues = new ArrayList<>(0);
+        
+        //Iterator it = wf.entrySet().iterator();
+        Iterator<WafFingerprint> it = wafFingerprints.iterator();
         while (it.hasNext()) {
-            Map.Entry pair = (Map.Entry)it.next();
-            stdout.println(pair.getKey() + " = " + pair.getValue());
+            WafFingerprint wfp = (WafFingerprint)it.next();            
+            //if (debug) stdout.println("DEBUG: Passive scan for WAF " + wfp.wafType + " (regex=" + wfp.regex + ")");
             
-            // look for matches of our passive check grep string
-            List<int[]> matches = getMatches(baseRequestResponse.getResponse(), helpers.stringToBytes(pair.getValue().toString()));
-            if (matches.size() > 0)
-            {
-                // report the issue
-                issues.add(new CustomScanIssue(
-                        baseRequestResponse.getHttpService(),
-                        helpers.analyzeRequest(baseRequestResponse).getUrl(), 
-                        new IHttpRequestResponse[] { callbacks.applyMarkers(baseRequestResponse, null, matches) }, 
-                        "WAF Detected",
-                        "The response contains the string: " + pair.getValue().toString(),
-                        "Information"));
-                //return issues;
+            // Get HTTP response
+            byte[] response      = baseRequestResponse.getResponse();
+            String responseTxt   = helpers.bytesToString(response);
+            URL url           = helpers.analyzeRequest(baseRequestResponse).getUrl();
+            
+            // Get the offset for the body and use it if HEADER_ONLY search is specified
+            int maxOffset = response.length;
+            if (wfp.header){
+                maxOffset = helpers.analyzeResponse(response).getBodyOffset();
             }
-            //else return null;
-            
-            it.remove(); // avoids a ConcurrentModificationException
+
+            // Perform a REGEX search
+            Pattern p = Pattern.compile(wfp.regex, Pattern.MULTILINE);
+            Matcher m = p.matcher(responseTxt.substring(0, maxOffset));
+            if (m.find()){
+                if (info) stdout.println("INFO: Match for WAF " + wfp.wafType + " at " + url.toString() + " (regex=" + wfp.regex + ";within " + maxOffset + " bytes of " + response.length + " bytes total)");
+                        
+                // look for matches of our passive check keyword
+                List<int[]> matches = getMatches(response, helpers.stringToBytes(wfp.keyword), maxOffset);
+                if (matches.size() > 0){
+                    // report the issue
+                    issues.add(new CustomScanIssue(
+                            baseRequestResponse.getHttpService(),
+                            url, 
+                            new IHttpRequestResponse[] { callbacks.applyMarkers(baseRequestResponse, null, matches) }, 
+                            "WAF Detected: " + wfp.wafType,
+                            "Fingerprint Details:" + wfp.describe(), 
+                            "Information"));
+                    if (info) stdout.println("INFO: Adding informational finding for WAF " + wfp.wafType + " (if not already present)");
+                }         
+                it.remove(); // avoids a ConcurrentModificationException
+            }                
         }   
         return issues;
     }
@@ -113,33 +136,6 @@ public class BurpExtender implements IBurpExtender, IScannerCheck
     @Override
     public List<IScanIssue> doActiveScan(IHttpRequestResponse baseRequestResponse, IScannerInsertionPoint insertionPoint)
     {
-        /*
-        // make a request containing our injection test in the insertion point
-        byte[] checkRequest = insertionPoint.buildRequest(INJ_TEST);
-        IHttpRequestResponse checkRequestResponse = callbacks.makeHttpRequest(
-                baseRequestResponse.getHttpService(), checkRequest);
-
-        // look for matches of our active check grep string
-        List<int[]> matches = getMatches(checkRequestResponse.getResponse(), INJ_ERROR);
-        if (matches.size() > 0)
-        {
-            // get the offsets of the payload within the request, for in-UI highlighting
-            List<int[]> requestHighlights = new ArrayList<>(1);
-            requestHighlights.add(insertionPoint.getPayloadOffsets(INJ_TEST));
-
-            // report the issue
-            List<IScanIssue> issues = new ArrayList<>(1);
-            issues.add(new CustomScanIssue(
-                    baseRequestResponse.getHttpService(),
-                    helpers.analyzeRequest(baseRequestResponse).getUrl(), 
-                    new IHttpRequestResponse[] { callbacks.applyMarkers(checkRequestResponse, requestHighlights, matches) }, 
-                    "Pipe injection",
-                    "Submitting a pipe character returned the string: " + helpers.bytesToString(INJ_ERROR),
-                    "High"));
-            return issues;
-        }
-        else return null;
-        */
         return null;
     }
 
@@ -160,6 +156,28 @@ public class BurpExtender implements IBurpExtender, IScannerCheck
             (existingIssue.getIssueDetail().equals(newIssue.getIssueDetail())))
             return -1;
         else return 0;
+    }
+    
+    /**
+     * Load WAF fingerprints from CSV file to simplify addition of new ones 
+     */
+    private void loadWafFingerprints(){
+        InputStream is = this.getClass().getClassLoader().getResourceAsStream(FP_FILENAME);
+        try {
+            InputStreamReader in = new InputStreamReader(is);
+            Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(in);
+            for (CSVRecord record : records) {
+                String waf = record.get("WAF_NAME").trim();
+                String kw  = record.get("KEYWORD").trim();
+                String re  = record.get("REGEX").trim();
+                String hdr = record.get("HEADER_ONLY").trim();
+                String ref = record.get("REFERENCE").trim();
+                String dsc = record.get("DESCRIPTION").trim();
+                wafFingerprints.add(new WafFingerprint(waf,kw, re, Boolean.valueOf(hdr), ref, dsc));
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace(stderr);
+        }        
     }
 }
 
@@ -259,14 +277,54 @@ class CustomScanIssue implements IScanIssue
     
 }
 
+/**
+ * WAF Fingerprint: Embedded class to hold information about WAF fingerprints to search for
+ * @author epaquet
+ */
 class WafFingerprint{
-    private byte[] keyword;
-    private String wafType;
-    //private String author;
-    //private String refUri;
+    String  wafType; 
+    String  keyword; 
+    String  regex;
+    String  ref;
+    String  descr;
+    Boolean header; 
     
-    public WafFingerprint(byte[] keyword, String wafType){
-        this.keyword = keyword;
+    /**
+     * Create WAF Fingerprint object
+     * @param wafType   Type of WAF to show in issue description
+     * @param keyword   Keyword to search for when highlighting text in HTTP Response
+     * @param regex     Regex to search for when checking for a WAF fingerprint
+     * @param header    Boolean to specify if only the HTTP response header should be analyzed
+     * @param ref       Reference information about where the WAF fingerprint research came from
+     * @param descr     Description for the WAF technology
+     */
+    public WafFingerprint(String wafType, String keyword, String regex, Boolean header, String ref, String descr){
         this.wafType = wafType;
+        this.keyword = keyword;
+        this.regex   = regex;
+        this.header  = header;
+        this.ref     = ref;
+        this.descr   = descr;
+    }
+
+    @Override
+    public String toString() {
+        String val = "";
+        val += "\n" + wafType;
+        val += "\t\t("   + header.toString() + ")";
+        //val += "\tkeyword=" + keyword;
+        val += "\t\t("   + regex + ")";
+        return val;
+    }
+
+    public String describe() {
+        String val = "\n";
+        val += "\nWAF Type            : " + wafType;
+        val += "\nWAF tech. details   : " + descr;
+        val += "\nReference           : " + ref;
+        val += "\nMatching regex      : " + regex;
+        val += "\nHighlighting keyword: " + keyword;
+        val += "\nHeader-only search? : " + header.toString();
+        return val;
     }
 }
